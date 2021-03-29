933a2698697975823e1d1c09efa98adf1e2e5afa
Minh Nguyen <thenguyen@paypal.com>
2021-03-29 17:49:57 -0400

    minor updates

--- Samples.Objc/Samples.ObjC/SceneDelegate.m
+++ Samples.Objc/Samples.ObjC/SceneDelegate.m
@@ -20,11 +20,6 @@
   // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
   // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
   // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-  
-  self.window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
-  CheckoutViewController *viewController = [[CheckoutViewController alloc] init];
-  self.window.rootViewController = viewController;
-  [self.window makeKeyAndVisible];
 }
 
 
